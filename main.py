418a1b59b9ab89ea644792f3add415b7a89b6b59
lvluo319 <32085396+lvluo319@users.noreply.github.com>
2019-10-15 10:35:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import os
-from gluoncv import model_zoo, data, utils
+from gluoncv import data, utils
 from mxnet import gluon
 import mxnet as mx
 import cv2
 
 def main():
     ctx = mx.cpu()
-
-    # net = model_zoo.get_model('yolo3_darknet53_voc', pretrained=False)
-
     classes = ['hat', 'person']
-    # initialize net
-    # for param in net.collect_params().values():
-    #     if param._data is not None:
-    #         continue
-    #     param.initialize()
-    # net.reset_class(classes)
-    # net.collect_params().reset_ctx(ctx)
-    #
-    # net.load_parameters('./model/darknet.params', ctx=ctx)
-
     net = gluon.SymbolBlock.imports(symbol_file='./model/darknet53-symbol.json', input_names=['data'],
                                     param_file='./model/darknet53-0000.params', ctx=ctx)
 
-
     no = 1
-    cap = cv2.VideoCapture('vedio.mp4')
-    # for file in os.listdir('./image'):
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-        # frame = './image/' + file
+    # cap = cv2.VideoCapture('vedio.mp4')
+    for file in os.listdir('./image'):
+    # while (cap.isOpened()):
+    #     ret, frame = cap.read()
+        frame = './image/' + file
         x, orig_img = data.transforms.presets.yolo.load_test(frame, short=416)
         x = x.as_in_context(ctx)
         box_ids, scores, bboxes = net(x)
@@ -42,8 +28,6 @@ def main():
             person_scores = [list_scores[i] for i in index_person]
             if max(person_scores) > 0.4:
                 # draw bounding boxes on pic.
-                # ax = utils.viz.cv_plot_bbox(orig_img, bboxes[0], scores[0], box_ids[0], class_names=net.classes,
-                #                             thresh=0.4)
                 ax = utils.viz.cv_plot_bbox(orig_img, bboxes[0], scores[0], box_ids[0], class_names=classes, thresh=0.4)
                 cv2.imwrite('./results/' + str(no) + '.jpg', orig_img[..., ::-1])
                 no += 1
